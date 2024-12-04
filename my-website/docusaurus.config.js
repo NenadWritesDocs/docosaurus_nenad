dcafa6be2aac632b41f7aae006adaf33fb93ea66
Nenad_writes_docs <nenadwritesdocs@gmail.com>
2024-12-04 23:31:39 +0100

    config.js updates

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -1,24 +1,10 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
-module.exports = {
-  url: 'https://nenadwritesdocs.netlify.app/', // Url to your site with no trailing slash
-  baseUrl: 'my-website', // Base directory of your site relative to your repo
-  // ...
-};
-
 
 const { themes } = require('prism-react-renderer');
-const githubTheme = themes.github;
-module.exports = {
-  // ... other config options
-  themeConfig: {
-    prism: {
-      theme: githubTheme,
-      // ... other prism options
-    },
-  },
-};
-
+const lightCodeTheme = themes.github;
+const darkCodeTheme = themes.dracula;
+const remarkableAdmonitions = require('remarkable-admonitions');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -26,26 +12,30 @@ const config = {
   tagline: 'Pet project documentation site',
   favicon: 'img/favicon.ico',
 
-  // Set the production url of your site here
-  url: 'https://nenadwritesdocs.netlify.app/',
+  // Set the production URL of your site here
+  url: 'https://nenadwritesdocs.netlify.app/', // Your website URL
   // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/', // Usually '/' unless you have a subpath
 
   // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
   organizationName: 'NenadWritesDocs', // Usually your GitHub org/user name.
   projectName: 'docusaurus_nenad', // Usually your repo name.
 
   onBrokenLinks: 'ignore',
   onBrokenMarkdownLinks: 'warn',
 
-  // Even if you don't use internalization, you can use this field to set useful
-  // metadata like html lang. For example, if your site is Chinese, you may want
-  // to replace "en" with "zh-Hans".
+  // Internationalization configuration
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
+  },
+
+  // Custom fields
+  customFields: {
+    markdownPlugins: [
+      // Highlight admonitions.
+      remarkableAdmonitions({ icon: 'svg-inline' }),
+    ],
   },
 
   presets: [
@@ -55,99 +45,97 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          // Add the new sidebar tab to the docs section
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
+          // Add any additional docs options here
         },
         blog: {
           showReadingTime: true,
           sortPosts: 'ascending',
           postsPerPage: 1,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
+          // Add any additional blog options here
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
+        },
+        // Include the remarkable-admonitions plugin
+        markdown: {
+          remarkPlugins: [remarkableAdmonitions],
         },
       }),
     ],
   ],
 
-  themeConfig: {
-    // Replace with your project's social card
-    image: 'img/docosaurus.mylogo.png',
-    navbar: {
-      title: 'Nenad Writes Docs',
-      logo: {
-        alt: 'My Site Logo',
-        src: 'img/docosaurus.mylogo.png',
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card image
+      image: 'img/docusaurus.mylogo.png',
+      navbar: {
+        title: 'Nenad Writes Docs',
+        logo: {
+          alt: 'My Site Logo',
+          src: 'img/docusaurus.mylogo.png',
+        },
+        items: [
+          {
+            type: 'docSidebar',
+            sidebarId: 'tutorialSidebar',
+            position: 'left',
+            label: 'CV and portfolio',
+          },
+          { to: '/blog', label: 'NYATWB', position: 'left' },
+          {
+            href: 'https://github.com/NenadWritesDocs',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
       },
-      items: [
-        {
-          type: 'docSidebar',
-          sidebarId: 'tutorialSidebar',
-          position: 'left',
-          label: 'CV and portfolio',
-        },
-
-
-        {to: '/blog', label: 'NYATWB', position: 'left'},
-        {
-          href: 'https://github.com/NenadWritesDocs',
-          label: 'GitHub',
-          position: 'right',
-        },
-      ],
-    },
-    footer: {
-      style: 'dark',
-      links: [
-        {
-          title: 'Docs',
-        
-        },
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Linkedin',
-              href: 'https://www.linkedin.com/in/nenad-pajovi%C4%87-127272159/',
-            },
-            {
-              label: 'Substack',
-              href: 'https://substack.com/profile/85055366-lako_polako?utm_source=user-menu',
-            },
-          ],
-        },
-        {
-          title: 'More',
-          items: [
-            {
-              label: 'Blog',
-              to: '/blog',
-            },
-            {
-              label: 'GitHub - Docosaurus repository',
-              href: 'https://github.com/facebook/docusaurus',
-            },
-          ],
-        },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} Nenad Writes Docs. Built with Docusaurus.`,
-    },
-    prism: {
-      theme: lightCodeTheme,
-      darkTheme: darkCodeTheme,
-    },
-  },
-
-  // Put custom fields here
-  customFields: {
-    markdownPlugins: [
-      // Highlight admonitions.
-      require('remarkable-admonitions')({ icon: 'svg-inline' })
-    ]
-  }
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'CV and Portfolio',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'LinkedIn',
+                href: 'https://www.linkedin.com/in/nenad-pajovi%C4%87-127272159/',
+              },
+              {
+                label: 'Substack',
+                href: 'https://substack.com/profile/85055366-lako_polako?utm_source=user-menu',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+              {
+                label: 'GitHub - Docusaurus Repository',
+                href: 'https://github.com/facebook/docusaurus',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Nenad Writes Docs. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
 };
 
 module.exports = config;
